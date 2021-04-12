d98e25f3395b7b67287466cbd2b2565a053a5d1d
barnrang <barnrang@gmail.com>
2021-04-12 21:13:16 +0900

    add script

--- alien_worlds.py
+++ alien_worlds.py
@@ -1,27 +1,73 @@
-from selenium import webdriver
-import chromedriver_binary
-import time
+from PIL import Image
+import io
+import numpy as np
 
-# options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-# driver = webdriver.Chrome(options=options)  # 今は chrome_options= ではなく options=
+import selenium
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-# driver.get('https://www.google.com/')
-# print(driver.title)
+import matplotlib.pyplot as plt
 
-# search_box = driver.find_element_by_name("q")
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-# print(driver.title)
+canvas = driver.find_element_by_id('unityContainer')
+style_element = [x.strip() for x in canvas.get_attribute('style').split(';')]
+width = int(style_element[0].split(' ')[-1][:-2])
+height = int(style_element[1].split(' ')[-1][:-2])
 
-# driver.save_screenshot('search_results.png')
-# driver.quit()
+mining_button_ref = plt.imread('alien_images/mining_button.png')
+claim_button_ref = plt.imread('alien_images/claim_button.png')
+return_to_mine_ref = plt.imread('alien_images/return_to_mine.png')
+
+def press_mining(posx=0, posy=0):
+    action = webdriver.common.action_chains.ActionChains(driver)
+    action.move_to_element_with_offset(canvas, posx, posy)
+    action.click()
+    action.perform()
+
+def check_if_can_mine():
+    data = driver.get_screenshot_as_png()
+    img = Image.open(io.BytesIO(data))
+    mining_button = np.asarray(img)[740:760, 600:650].astype(np.float32) / 255.
+    return (np.mean(np.abs(mining_button_ref - mining_button)) < 0.01)
+
+def check_if_can_claim():
+    data = driver.get_screenshot_as_png()
+    img = Image.open(io.BytesIO(data))
+    mining_button = np.asarray(img)[400:450, 400:450].astype(np.float32) / 255.
+    return (np.mean(np.abs(claim_button_ref - mining_button)) < 0.01)
+
+def check_if_can_return():
+    data = driver.get_screenshot_as_png()
+    img = Image.open(io.BytesIO(data))
+    mining_button = np.asarray(img)[680:700, 250:300].astype(np.float32) / 255.
+    return (np.mean(np.abs(return_to_mine_ref - mining_button)) < 0.01)
+
+mining_pos = (510, 600)
+claim_pos = (510, 400)
+return_to_mining_pos = (200, 560)
 
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-driver.get('https://play.alienworlds.io/');
-time.sleep(1000) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
+driver.get('https://play.alienworlds.io/')
+print('Please login and enter the page before press mining')
+print('Enter "y" and press enter if ready: ')
+while input() != 'y':
+    print('Please enter "y" and press enter if ready whenever you are ready:)))): ')
+
+while True:
+    print('Prepare to mine')
+    while not check_if_can_mine():
+        time.sleep(20)
+    press_mining(*mining_pos)
+    
+    print('Prepare to claim')
+    while not check_if_can_claim():
+        time.sleep(20)
+    press_mining(*claim_pos)
+    
+    print('Prepare to return')
+    while not check_if_can_return():
+        time.sleep(20)
+    press_mining(*return_to_mining)
+
+
 driver.quit()
